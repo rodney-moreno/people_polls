6ca0d41df86310c877b9b546dde02ba5cb96a0e0
Rodney Moreno <rodney.moreno@outlook.com>
2022-11-22 15:33:58 -0800

    Initial Project Structure

---
+++ frontend-max/app.js
@@ -0,0 +1,35 @@
+const http = require('node:http');
+const fs = require('node:fs');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        fs.readFile('index.html', (err, data) => {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(data);
+            res.end();
+        });
+    } else if (req.url === '/login') {
+        fs.readFile('login.html', (err, data) => {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(data);
+            res.end();
+        });
+    } else if (req.url === '/register') {
+        fs.readFile('register.html', (err, data) => {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(data);
+            res.end();
+        });
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text' });
+        res.write("Page does not exist");
+        res.end();
+    }
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
